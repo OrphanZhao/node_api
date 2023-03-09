16607dac814fdc38d2fd2fc3e51a06e4d5d166a3
orphanzhao <class_moon@163.com>
2023-03-09 16:39:34 +0800

    feat: __dirname

--- fs/score.js
+++ fs/score.js
@@ -3,4 +3,11 @@ const fs = require("fs");
 /**
  * 路径动态拼接问题
  */
-fs.readFile("./score.txt", "utf8", (err, data) => console.log(err.message));
+
+fs.readFile(
+  `${__dirname}/score.txt`,
+  "utf8",
+  (err) => err && console.log(err.message)
+);
+
+console.group(__dirname);
